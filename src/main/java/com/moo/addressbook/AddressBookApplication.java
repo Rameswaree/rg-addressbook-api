872849de73dc44c463550023d79778b34c562f6b
Rameswaree <43037382+Rameswaree@users.noreply.github.com>
2020-01-18 09:45:28 +0000

    Created AddressBookApplication.java

---
+++ src/main/java/com/moo/addressbook/AddressBookApplication.java
@@ -0,0 +1,11 @@
+package com.moo.addressbook;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class AddressBookApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+}
