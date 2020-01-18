9c0e389aca21b51fd33caf1f56c7966edfaf8141
Rameswaree Ganesh <rameswaree@gmail.com>
2020-01-18 13:34:17 +0000

    Made the ids static

--- src/main/java/com/moo/addressbook/adaptor/CustomerAdaptorImpl.java
+++ src/main/java/com/moo/addressbook/adaptor/CustomerAdaptorImpl.java
@@ -9,24 +9,19 @@ import java.util.List;
 @Component
 public class CustomerAdaptorImpl implements CustomerAdaptor {
 
-    private long id = 0;
     @Override
     public List<Customer> createCustomerAddressList() {
         List<Customer> customerList = new ArrayList<>();
-        customerList.add(new Customer(incrementId(),"Reemu","Ganesh","6","EastBrook Hall","","","Bradford","UK","BD1 5AE"));
-        customerList.add(new Customer(incrementId(),"Srikanth","Shekar","6","EastBrook Hall","","","Bradford","UK","BD1 5AE"));
-        customerList.add(new Customer(incrementId(),"Mahesh","Shekar","7","Gatehaus","","","Leeds","UK","LS1 4PE"));
-        customerList.add(new Customer(incrementId(),"Chandra","Shekar","8","Landmark","","","Halifax","UK","HX1 1AD"));
-        customerList.add(new Customer(incrementId(),"Sivagamy","Viswa","8","Landmark","","","Halifax","UK","HX1 1AD"));
-        customerList.add(new Customer(incrementId(),"Sarasa","Raman","9","Broadway","","","Skipton","UK","BD23 1EP"));
-        customerList.add(new Customer(incrementId(),"Gachu","Thyagu","9","Broadway","","","Skipton","UK","BD23 1EP"));
-        customerList.add(new Customer(incrementId(),"Padma","Ganapathy","10","EastBrook Hall","","","Manchester","UK","M1 1AJ"));
-        customerList.add(new Customer(incrementId(),"Rajan","Swamy","11","EastBrook Hall","","","Manchester","UK","M1 1AJ"));
-        customerList.add(new Customer(incrementId(),"Krishna","Venkat","12","EastBrook Hall","","","London","UK","E1 7AE"));
+        customerList.add(new Customer(1,"Reemu","Ganesh","6","EastBrook Hall","","","Bradford","UK","BD1 5AE"));
+        customerList.add(new Customer(2,"Srikanth","Shekar","6","EastBrook Hall","","","Bradford","UK","BD1 5AE"));
+        customerList.add(new Customer(3,"Mahesh","Shekar","7","Gatehaus","","","Leeds","UK","LS1 4PE"));
+        customerList.add(new Customer(4,"Chandra","Shekar","8","Landmark","","","Halifax","UK","HX1 1AD"));
+        customerList.add(new Customer(5,"Sivagamy","Viswa","8","Landmark","","","Halifax","UK","HX1 1AD"));
+        customerList.add(new Customer(6,"Sarasa","Raman","9","Broadway","","","Skipton","UK","BD23 1EP"));
+        customerList.add(new Customer(7,"Gachu","Thyagu","9","Broadway","","","Skipton","UK","BD23 1EP"));
+        customerList.add(new Customer(8,"Padma","Ganapathy","10","EastBrook Hall","","","Manchester","UK","M1 1AJ"));
+        customerList.add(new Customer(9,"Rajan","Swamy","11","EastBrook Hall","","","Manchester","UK","M1 1AJ"));
+        customerList.add(new Customer(10,"Krishna","Venkat","12","EastBrook Hall","","","London","UK","E1 7AE"));
         return customerList;
-    }
-
-    private long incrementId(){
-        return id++;
     }
 }
